2dff9837861464380de4eba93fdb39e8781a37ed
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-10-11 14:39:00 +0100

    fix naming convention

--- smooth-line.cpp
+++ smooth-line.cpp
@@ -11,18 +11,18 @@ void SmoothLine::draw(const as::vec3& begin, const as::vec3& end)
   const auto line_length = as::vec_distance(begin, end);
   for (auto segment = 0; segment < line_granularity; ++segment) {
     const float scale = 1.0f;
-    float segmentBegin = segment / float(line_granularity);
-    float segmentEnd = float(segment + 1) / float(line_granularity);
+    float segment_begin = segment / float(line_granularity);
+    float segment_end = float(segment + 1) / float(line_granularity);
 
     debug_lines_.addLine(
       nlt::bezier5(
         begin, end, begin + as::vec3::axis_x(scale),
         begin + as::vec3::axis_x(scale), end - as::vec3::axis_x(scale),
-        end - as::vec3::axis_x(scale), segmentBegin),
+        end - as::vec3::axis_x(scale), segment_begin),
       nlt::bezier5(
         begin, end, begin + as::vec3::axis_x(scale),
         begin + as::vec3::axis_x(scale), end - as::vec3::axis_x(scale),
-        end - as::vec3::axis_x(scale), segmentEnd),
+        end - as::vec3::axis_x(scale), segment_end),
       0xffffffff);
   }
 
