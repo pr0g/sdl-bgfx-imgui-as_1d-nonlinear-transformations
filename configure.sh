34c051223a03c460217392f633faa121b5a02864
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-12-02 22:35:46 +0000

    update configure.sh to use new third-party build structure

--- configure.sh
+++ configure.sh
@@ -2,15 +2,9 @@
 
 # command to configure this repo for building after dependencies have been installed
 cmake -S . -B build/debug -G Ninja -DCMAKE_BUILD_TYPE=Debug \
--DCMAKE_PREFIX_PATH=\
-"$(pwd)/third-party/libs/SDL2-2.0.12/install;\
-$(pwd)/third-party/libs/bgfx/install;\
-$(pwd)/third-party/libs/imgui/install" \
+-DCMAKE_PREFIX_PATH="$(pwd)/third-party/build" \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
-cmake -S . -B build/release -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo \
--DCMAKE_PREFIX_PATH=\
-"$(pwd)/third-party/libs/SDL2-2.0.12/install;\
-$(pwd)/third-party/libs/bgfx/install;\
-$(pwd)/third-party/libs/imgui/install" \
+cmake -S . -B build/release -G Ninja -DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_PREFIX_PATH="$(pwd)/third-party/build" \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
