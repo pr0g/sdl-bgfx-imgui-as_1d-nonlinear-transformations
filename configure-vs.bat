31c05618196ed1753e8b9d76ea0c4a957fafd891
Hulton-Harrop <hultonha@amazon.co.uk>
2020-12-02 13:24:32 +0000

    update configure-vs.bat to use new third-party path

--- configure-vs.bat
+++ configure-vs.bat
@@ -2,7 +2,4 @@
 
 REM command to configure this repo for building after dependencies have been installed
 cmake -S . -B build-vs -G "Visual Studio 16 2019" -A x64 ^
--DCMAKE_PREFIX_PATH=^
-%cd%/third-party/libs/SDL2-2.0.12/install;^
-%cd%/third-party/libs/bgfx/install;^
-%cd%/third-party/libs/imgui/install
+-DCMAKE_PREFIX_PATH=%cd%/third-party/build
