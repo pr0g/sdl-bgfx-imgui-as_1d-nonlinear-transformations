cfa60cbf53f49cd3c3f3c7aaa247228ad0ce131d
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-12-12 19:44:27 +0000

    update compile-shaders.sh to use the correct path

--- compile-shaders.sh
+++ compile-shaders.sh
@@ -3,19 +3,19 @@
 # compile shaders
 
 # simple shader
-./third-party/libs/bgfx/install/bin/shaderc  \
+./third-party/build/bin/shaderc \
 -f shader/simple/v_simple.sc -o shader/simple/v_simple.bin  \
 --platform osx --type vertex --verbose -i ./ -p metal
 
-./third-party/libs/bgfx/install/bin/shaderc \
+./third-party/build/bin/shaderc \
 -f shader/simple/f_simple.sc -o shader/simple/f_simple.bin \
 --platform osx --type fragment --verbose -i ./ -p metal
 
 # instance shader
-./third-party/libs/bgfx/install/bin/shaderc  \
+./third-party/build/bin/shaderc \
 -f shader/instance/v_instance.sc -o shader/instance/v_instance.bin  \
 --platform osx --type vertex --verbose -i ./ -p metal
 
-./third-party/libs/bgfx/install/bin/shaderc \
+./third-party/build/bin/shaderc \
 -f shader/instance/f_instance.sc -o shader/instance/f_instance.bin \
 --platform osx --type fragment --verbose -i ./ -p metal
