d3f6000ebb4ca003885a808f83ad1c17f7562651
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-12-12 20:15:36 +0000

    small tidy-up to configure scripts

--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# command to configure this repo for building after dependencies have been installed
+# Commands to configure this repo for building after dependencies have been installed
+
 cmake -S . -B build/debug -G Ninja -DCMAKE_BUILD_TYPE=Debug \
 -DCMAKE_PREFIX_PATH="$(pwd)/third-party/build" \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
