ce8ba356916dbd279a6e0a160f0968ad0e5e78b7
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-01-27 21:10:28 +0000

    undo path change in configure script

--- configure.bat
+++ configure.bat
@@ -3,11 +3,11 @@
 REM command to configure this repo for building after dependencies have been installed
 
 cmake -S . -B build/debug -G Ninja -DCMAKE_BUILD_TYPE=Debug ^
--DCMAKE_PREFIX_PATH=%cd%/../sdl-bgfx-imgui-starter/third-party/build ^
+-DCMAKE_PREFIX_PATH=%cd%/third-party/build ^
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
 -DAS_PRECISION_FLOAT=ON -DAS_COL_MAJOR=ON
 
 cmake -S . -B build/release -G Ninja -DCMAKE_BUILD_TYPE=Release ^
--DCMAKE_PREFIX_PATH=%cd%/../sdl-bgfx-imgui-starter/third-party/build ^
+-DCMAKE_PREFIX_PATH=%cd%/third-party/build ^
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
 -DAS_PRECISION_FLOAT=ON -DAS_COL_MAJOR=ON
