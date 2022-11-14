c6221089c2c7c64b378b9911bed4ecf8839e05b8
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2022-11-13 20:26:50 -0800

    update configure scripts

--- configure-vs.bat
+++ configure-vs.bat
@@ -2,6 +2,6 @@
 
 REM Command to configure this repo for building after dependencies have been installed
 
-cmake -S . -B build-vs -G "Visual Studio 16 2019" -A x64 ^
+cmake -B build-vs -G "Visual Studio 17 2022" ^
 -DCMAKE_PREFIX_PATH=%cd%/third-party/build ^
 -DAS_PRECISION_FLOAT=ON -DAS_COL_MAJOR=ON
--- configure.bat
+++ configure.bat
@@ -2,12 +2,12 @@
 
 REM command to configure this repo for building after dependencies have been installed
 
-cmake -S . -B build/debug -G Ninja -DCMAKE_BUILD_TYPE=Debug ^
+cmake -B build/debug -G Ninja -DCMAKE_BUILD_TYPE=Debug ^
 -DCMAKE_PREFIX_PATH=%cd%/third-party/build ^
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
 -DAS_PRECISION_FLOAT=ON -DAS_COL_MAJOR=ON
 
-cmake -S . -B build/release -G Ninja -DCMAKE_BUILD_TYPE=Release ^
+cmake -B build/release -G Ninja -DCMAKE_BUILD_TYPE=Release ^
 -DCMAKE_PREFIX_PATH=%cd%/third-party/build ^
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
 -DAS_PRECISION_FLOAT=ON -DAS_COL_MAJOR=ON
