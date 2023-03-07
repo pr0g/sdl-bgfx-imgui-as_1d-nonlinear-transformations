a087587883350af7c504f34e1b343641f5f65689
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2023-03-06 22:45:16 -0800

    updates/fixes for windows (directx)

--- compile-shaders-win.bat
+++ compile-shaders-win.bat
@@ -19,3 +19,13 @@ third-party\build\bin\shaderc.exe ^
 third-party\build\bin\shaderc.exe ^
 -f shader\next\f_next.sc -o shader\next\f_next.bin ^
 --platform windows --type fragment --verbose -i ./ -p ps_5_0
+
+REM basic (normal) lighting shader
+third-party\build\bin\shaderc.exe ^
+-f shader\basic-lighting\v_basic.sc -o shader\basic-lighting\v_basic.bin ^
+--platform windows --type vertex --verbose -i ./ -p vs_5_0
+
+third-party\build\bin\shaderc.exe ^
+-f shader/basic-lighting/f_basic.sc -o shader/basic-lighting/f_basic.bin ^
+--platform windows --type fragment --verbose -i ./ -p ps_5_0
+
